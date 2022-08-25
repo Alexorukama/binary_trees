ac7275c674d699fa18476bf09b8d131b24bd1bd5
Ayomide Suara <aysuarex@gmail.com>
2022-08-25 16:51:00 -0700

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,44 +1,12 @@
 #include "binary_trees.h"
+
 /**
- * heap_extract - extracts the root node from a Max Binary Heap
- * @root: pointer to the heap root
- * Return: value of extracted node
- **/
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
+ *
+ * @root: double pointer to the root node of heap
+ * Return: value stored in the root node
+ */
 int heap_extract(heap_t **root)
 {
-	int value, value_aux;
-	heap_t *aux;
-
-	if (!root || !*root)
-		return (0);
-	aux = *root;
-	value = aux->n;
-	if (!aux->left && !aux->right && !aux->parent)
-	{
-		free(aux);
-		return (value);
-	}
-	while (aux->left || aux->right)
-	{
-		if (aux->left && aux->right)
-		{
-			if (!aux->right || aux->left->n > aux->right->n)
-			{
-				value_aux = aux->n;
-				aux->n = aux->left->n;
-				aux->left->n = value_aux;
-				aux = aux->left;
-			}
-			else if (aux->left->n < aux->right->n)
-			{
-				value_aux = aux->n;
-				aux->n = aux->right->n;
-				aux->right->n = value_aux;
-				aux = aux->right;
-
-			}
-		}
-
-	}
-	return (value);
+	return (root);
 }
